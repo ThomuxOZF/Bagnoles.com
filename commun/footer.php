e44865c50f3cbe570164c90bf862bd0159b2c2fc
ThomuxOZF <ruliaua@gmail.com>
2024-06-07 14:00:40 +0200

    Add files via upload

---
+++ commun/footer.php
@@ -0,0 +1,33 @@
+<footer>
+        <section class="fromage">
+              <div class="biere">
+                    <h3>Liens Utiles</h3>
+                      <ul>
+                          <li>Nos bagnoles</li>
+                          <li>Catégories</li>
+                          <li>Nous contacter</li>
+                          <li>Votre compte </li>
+                          <li>Conditions d'achats</li>
+                          <li>Politique de l'entreprise</li>
+                      </ul>
+                </div>
+                <div class="biere">
+                    <h2>Newsletters</h2>
+                    <input type="text" class="grosse-input" placeholder="Mail">
+                    <button class="abt" href="#">Subscribe Now</button>
+                </div>      
+              <div class="biere">
+                    <h3>Contact</h3>
+                    <p>2503 Williams Mine Road</p>
+                    <p>Alaska,99742</p>
+                  <div class="cidre">
+                    <i class="fa fa-instagram"></i>
+                    <i class="fa fa-twitter"></i>
+                    <i class="fa fa-facebook"></i>
+                  </div>
+              </div>
+        </section>
+    </footer>
+    <script src="asset/js/bagnoles.js"></script>
+  </body>
+</html>
---
+++ commun/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Accueil</title>
+        <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+        <link rel="stylesheet" href="asset/css/accueil.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Sonsie+One&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+  <body>
+    <header>
+        <nav class="logo">     
+            <a href="accueil.php"><img id="log" src="asset/img/bagnolelogo.png" alt="logobagnoles"></a>
+            <ul id="menu">
+                <li><button id="menubutton">Menu</button>
+                    <ul class="deroulant">
+                        <li><a href="accueil.php"><img class="logobagnole" src="asset/img/bagnolelogo.png" alt="logobagnoles"></a></li>
+                        <li><hr></li>
+                        <li><a href="accueil.php"><button class="button_menu">Nos Bagnoles</button></a></li>
+                        <li><button class="button_menu">Catégories</button></li>
+                        <li><button class="button_menu">Nous Contacter</button></li>                       
+                        <li><a href="profil.php"><button class="button_menu">Votre Compte</button></a></li>
+                        <li><a href="inscription.php"><button class="button_menu">Créer Un Compte</button></a></li>
+                        <li><button class="button_menu">Conditions d'Achat</button></li>
+                        <li>Politique de l'entrepise</li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </header>
+   
